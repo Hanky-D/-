70375d67c3988ad6d33a537214c9808e5614dc35
Hanky-D <dhjzxxdworkmail@163.com>
2021-03-31 00:05:17 +0800

    my

---
+++ pages/my/my.js
@@ -0,0 +1,75 @@
+Page({
+  data: {
+    week:["周一","周二","周三","周四","周五"],
+    banci:["第一批","第二批"],
+    selweek:"周一",
+    selbanci:"第一批",
+    weekval:0,
+    bancival:[0],
+  },
+  pickchange(e){
+    this.setData({
+      weekval:e.detail.value
+    })
+  },
+  formReset(e){
+    this.setData({
+      weekval:0
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
